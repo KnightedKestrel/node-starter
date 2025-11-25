4b9fb8482b09ea805fcf307e0ff36f9f960a7965
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-11-24 22:06:42 -0600

    ESLint and Prettier

---
+++ eslint.config.ts
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          trailingComma: 'es5',
+          tabWidth: 2,
+          printWidth: 80,
+        },
+      ],
+    },
+  },
+]);
